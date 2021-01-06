be8ce158ba4103e602ed7c8a556788fd3540e6c1
xurf <375634210@qq.com>
2021-01-06 17:18:34 +0800

    green_image_block

--- config/filesystems.php
+++ config/filesystems.php
@@ -74,7 +74,7 @@ return [
             'endpoint'   => env('OSS_ENDPOINT','oss-cn-shanghai.aliyuncs.com'), // 使用 ssl 这里设置如: https://oss-cn-beijing.aliyuncs.com
             'bucket'     => env('OSS_BUCKET','fun-touch'),
             'isCName'    => env('OSS_IS_CNAME', false), // 如果 isCname 为 false，endpoint 应配置 oss 提供的域名如：`oss-cn-beijing.aliyuncs.com`，否则为自定义域名，，cname 或 cdn 请自行到阿里 oss 后台配置并绑定 bucket
-            'domain_url' => env('OSS_IMG_DOMAIN','https://resource.funtouchpal.com/'),
+            'domain_url' => env('OSS_IMG_DOMAIN','http://fun-touch.oss-cn-shanghai.aliyuncs.com/'),//'https://resource.funtouchpal.com/'
         ],
 
     ],
