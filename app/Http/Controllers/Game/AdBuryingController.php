c14c48b65d0a92194144884cb44a87e9ef9b7087
xurf <375634210@qq.com>
2021-01-19 18:25:19 +0800

    burying game_package_id bug

--- app/Http/Controllers/Game/AdBuryingController.php
+++ app/Http/Controllers/Game/AdBuryingController.php
@@ -15,13 +15,13 @@ class AdBuryingController extends \App\Base\Controllers\Controller
     public function report(Request $request)
     {
         $this->validate($request,[
-            'package_id' => ['required', Rule::exists('game_package','id')],
+            'game_package_id' => ['required', Rule::exists('game_package','id')],
             'type' => ['required',Rule::in([1, 2]),],
             'device_uid' => 'required',
             'show_type' => 'required',
         ]);
         return app(AdBuryingService::class)->report(
-            $request->package_id,
+            $request->game_package_id,
             $request->type,
             $request->device_uid,
             $request->show_type,
