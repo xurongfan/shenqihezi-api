9c74282e8a6d6403e5b4bebaabaa9e636454a4a0
xurf <375634210@qq.com>
2021-01-26 18:25:35 +0800

    导入数据清洗

--- routes/api.php
+++ routes/api.php
@@ -404,6 +404,19 @@ Route::any('/topic-comment', function () {
         'Looks awesome!',
         'Yum',
         'That looks sooooooo yummy!!!',
+        'Hi',
+        'Yum!',
+        ' This looks so good!!',
+        'so good!',
+        'Awe!',
+        'Fantastic',
+        'So funny',
+        'Omg!',
+        'Hey',
+        'OhOhOh',
+        'funny!!!',
+        'Lol funny!!!',
+        'Very funny!!!',
         '😄😂',
         '😘',
         '￣▽￣🤯',
@@ -455,30 +468,34 @@ Route::any('/topic-comment', function () {
          $item = $item->toArray();
          foreach ($item as $key => $value){
              $userArr = [];
-             //评论入库
-             foreach ($contentArr as $comment){
-                 $userId = rand(9990,10187);//rand(9990,10085);
-                 \App\Models\Topic\TopicContentComment::query()->insert([
-                     'content_id' => $value['id'],
-                     'comment' => $comment,
-                     'mer_user_id' => $userId,
-                     'created_at' => date('Y-m-d H:i:s',time()-(rand(1,100000))),
-                     'updated_at' => date('Y-m-d H:i:s',time())
-                 ]);
-                 $userArr[] = $userId;
+             if ($value['id']%3 == 0 || $value['id']%7 == 0)
+             {
+                 //评论入库
+                 foreach ($contentArr as $comment){
+                     $userId = rand(9990,10187);//rand(9990,10085);
+                     \App\Models\Topic\TopicContentComment::query()->insert([
+                         'content_id' => $value['id'],
+                         'comment' => $comment,
+                         'mer_user_id' => $userId,
+                         'created_at' => date('Y-m-d H:i:s',time()-(rand(1,100000))),
+                         'updated_at' => date('Y-m-d H:i:s',time())
+                     ]);
+                     $userArr[] = $userId;
+                 }
              }
 
-             if ($userArr){
-                 foreach ($userArr as $user){
-                     \App\Models\Topic\TopicContentLike::query()->firstOrCreate([
-                         'content_id' => $value['id'],
-                         'mer_user_id' => $user
-                     ]);
-                     \App\Models\User\MerUserFollow::query()->firstOrCreate([
-                         'follow_user_id' => $value['mer_user_id'],
-                         'mer_user_id' => $user
-                     ]);
-                 }
+             for ($i = 0;$i<$value['id']%10;$i++){
+                 $userArr[] = rand(9990,10187);
+             }
+             foreach ($userArr as $user){
+                 \App\Models\Topic\TopicContentLike::query()->firstOrCreate([
+                     'content_id' => $value['id'],
+                     'mer_user_id' => $user
+                 ]);
+                 \App\Models\User\MerUserFollow::query()->firstOrCreate([
+                     'follow_user_id' => $value['mer_user_id'],
+                     'mer_user_id' => $user
+                 ]);
              }
 
              return 'success';
