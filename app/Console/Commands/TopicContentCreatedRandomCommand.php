d726bacab1fcbec513b5e4dec732a8818438107c
xurf <375634210@qq.com>
2021-02-02 17:48:02 +0800

    清洗导入数据时间排序

--- app/Console/Commands/TopicContentCreatedRandomCommand.php
+++ app/Console/Commands/TopicContentCreatedRandomCommand.php
@@ -40,7 +40,7 @@ class TopicContentCreatedRandomCommand extends Command
     {
         \App\Models\Topic\TopicContent::query()
             ->where('mer_user_id',10271)
-            ->where('id','<=',1290)
+            ->where('id','<=',1375)
             ->where('is_export',1)->chunkById(100,function ($item){
                 $item = $item->toArray();
                 foreach ($item as $key => $value){
