8029e3eb571567df02dd45423b6c4a465d5e3d69
xurf <375634210@qq.com>
2021-03-05 16:47:23 +0800

    发布时间清洗

--- routes/api.php
+++ routes/api.php
@@ -408,128 +408,132 @@ Route::any('/topic-comment', function () {
      \App\Models\Topic\TopicContent::query()->where('is_export',1)->chunk(50,function ($item){
          $item = $item->toArray();
          foreach ($item as $key => $value){
-             $content = [
-                 'Nah..........hhhh',
-                 'So freaking cure!!!!',
-                 'Crazy',
-                 'that\'s funny',
-                 'i love it ',
-                 'GOOD',
-                 'Hahaha',
-                 'Lol',
-                 'Yep!',
-                 'Wow …',
-                 'pretty!',
-                 'Amazing',
-                 'Lol that\'s funny',
-                 'Brilliant!',
-                 ' It\'s funny',
-                 'Hell yeah!!!',
-                 'Hahahaha!!!',
-                 'Great',
-                 'Oh hell yes.',
-                 'Nice',
-                 'Crazy!',
-                 'Fantastic',
-                 'That’s so amazing',
-                 'So beautiful.',
-                 'That\'s beautiful!!',
-                 'So pretty!',
-                 'Looks so good!!!',
-                 'Oh wow that looks really really good!',
-                 'Looks amazing',
-                 'Looks awesome!',
-                 'Yum',
-                 'That looks sooooooo yummy!!!',
-                 'Hi',
-                 'Yum!',
-                 ' This looks so good!!',
-                 'so good!',
-                 'Awe!',
-                 'Fantastic',
-                 'So funny',
-                 'Omg!',
-                 'Hey',
-                 'OhOhOh',
-                 'funny!!!',
-                 'Lol funny!!!',
-                 'Very funny!!!',
-                 '😄😂',
-                 '😘',
-                 '￣▽￣🤯',
-                 '😍😍😍😍😍',
-                 '👍👍👍',
-                 '👏👏👏',
-                 '😃😃😃',
-                 '😂😂😂',
-                 '🙂',
-                 '😋😋😋',
-                 '🥰',
-                 '🥰🥰🥰',
-                 'Omg🤣🤣🤣',
-                 'OMG😂😂😂',
-                 'So funny🤣🤣🤣',
-                 'Fantastic!👏👏👏',
-                 'Awe!😋😋',
-                 'So good!😝😝',
-                 'This looks so good!😎😎',
-                 'Yum!🥰🥰🥰🥰',
-                 'Love it!🥰🥰🥰',
-                 'Hi😃',
-                 'Crazy🥳🥳🥳',
-                 'Nice👏👏👏',
-                 'Oh hell yes.😃😃😃',
-                 'Great👍👍👍',
-                 'Hell yeah!!!🙂🙂🙂',
-                 'YEAL👏👏👏',
-                 'It\'s funny😎😎😎',
-                 'Lol that\'s funny😂😂😂',
-                 'Amazing😋😋',
-                 'pretty!😆',
-                 '🤓🤓🤓',
-                 'Wow.....🤓🤓🤓🤓',
-                 'Lol that\'s funny😂😂😂',
-                 'Hahaha🥰🥰🥰',
-                 'This looks so good!😎😎',
-                 '😎😎😎😋😋😋',
-                 '😜😜😝😝😝',
-                 '❤️❤️',
-                 '❤️',
-                 '😊😊😊',
-                 '😊😊'
-             ];
-             $contentArr = array_random($content,rand(1,count($content)-1));
-             shuffle($contentArr);
-             $userArr = [];
-             if ($value['id']%3 == 0 || $value['id']%7 == 0)
-             {
-                 //评论入库
-                 foreach ($contentArr as $comment){
-                     $userId = rand(9990,10187);//rand(9990,10085);
-                     \App\Models\Topic\TopicContentComment::query()->insert([
-                         'content_id' => $value['id'],
-                         'comment' => $comment,
-                         'mer_user_id' => $userId,
-                         'created_at' => date('Y-m-d H:i:s',time()-(rand(1,100000))),
-                         'updated_at' => date('Y-m-d H:i:s',time())
-                     ]);
-                     $userArr[] = $userId;
-                 }
-             }
 
-             for ($i = 0;$i<$value['id']%10;$i++){
-                 $userArr[] = rand(9990,10187);
-             }
-             foreach ($userArr as $user){
-                 \App\Models\Topic\TopicContentLike::query()->firstOrCreate([
-                     'content_id' => $value['id'],
-                     'mer_user_id' => $user
-                 ]);
-                 \App\Models\User\MerUserFollow::query()->firstOrCreate([
-                     'follow_user_id' => $value['mer_user_id'],
-                     'mer_user_id' => $user
-                 ]);
-             }
+             \App\Models\Topic\TopicContent::query()->where('id',$value['id'])->update([
+                 'created_at' => date('Y-m-d H:i:s',time()-rand(60*30,60*60*7*24))
+             ]);
+//             $content = [
+//                 'Nah..........hhhh',
+//                 'So freaking cure!!!!',
+//                 'Crazy',
+//                 'that\'s funny',
+//                 'i love it ',
+//                 'GOOD',
+//                 'Hahaha',
+//                 'Lol',
+//                 'Yep!',
+//                 'Wow …',
+//                 'pretty!',
+//                 'Amazing',
+//                 'Lol that\'s funny',
+//                 'Brilliant!',
+//                 ' It\'s funny',
+//                 'Hell yeah!!!',
+//                 'Hahahaha!!!',
+//                 'Great',
+//                 'Oh hell yes.',
+//                 'Nice',
+//                 'Crazy!',
+//                 'Fantastic',
+//                 'That’s so amazing',
+//                 'So beautiful.',
+//                 'That\'s beautiful!!',
+//                 'So pretty!',
+//                 'Looks so good!!!',
+//                 'Oh wow that looks really really good!',
+//                 'Looks amazing',
+//                 'Looks awesome!',
+//                 'Yum',
+//                 'That looks sooooooo yummy!!!',
+//                 'Hi',
+//                 'Yum!',
+//                 ' This looks so good!!',
+//                 'so good!',
+//                 'Awe!',
+//                 'Fantastic',
+//                 'So funny',
+//                 'Omg!',
+//                 'Hey',
+//                 'OhOhOh',
+//                 'funny!!!',
+//                 'Lol funny!!!',
+//                 'Very funny!!!',
+//                 '😄😂',
+//                 '😘',
+//                 '￣▽￣🤯',
+//                 '😍😍😍😍😍',
+//                 '👍👍👍',
+//                 '👏👏👏',
+//                 '😃😃😃',
+//                 '😂😂😂',
+//                 '🙂',
+//                 '😋😋😋',
+//                 '🥰',
+//                 '🥰🥰🥰',
+//                 'Omg🤣🤣🤣',
+//                 'OMG😂😂😂',
+//                 'So funny🤣🤣🤣',
+//                 'Fantastic!👏👏👏',
+//                 'Awe!😋😋',
+//                 'So good!😝😝',
+//                 'This looks so good!😎😎',
+//                 'Yum!🥰🥰🥰🥰',
+//                 'Love it!🥰🥰🥰',
+//                 'Hi😃',
+//                 'Crazy🥳🥳🥳',
+//                 'Nice👏👏👏',
+//                 'Oh hell yes.😃😃😃',
+//                 'Great👍👍👍',
+//                 'Hell yeah!!!🙂🙂🙂',
+//                 'YEAL👏👏👏',
+//                 'It\'s funny😎😎😎',
+//                 'Lol that\'s funny😂😂😂',
+//                 'Amazing😋😋',
+//                 'pretty!😆',
+//                 '🤓🤓🤓',
+//                 'Wow.....🤓🤓🤓🤓',
+//                 'Lol that\'s funny😂😂😂',
+//                 'Hahaha🥰🥰🥰',
+//                 'This looks so good!😎😎',
+//                 '😎😎😎😋😋😋',
+//                 '😜😜😝😝😝',
+//                 '❤️❤️',
+//                 '❤️',
+//                 '😊😊😊',
+//                 '😊😊'
+//             ];
+//             $contentArr = array_random($content,rand(1,count($content)-1));
+//             shuffle($contentArr);
+//             $userArr = [];
+//             if ($value['id']%3 == 0 || $value['id']%7 == 0)
+//             {
+//                 //评论入库
+//                 foreach ($contentArr as $comment){
+//                     $userId = rand(9990,10187);//rand(9990,10085);
+//                     \App\Models\Topic\TopicContentComment::query()->insert([
+//                         'content_id' => $value['id'],
+//                         'comment' => $comment,
+//                         'mer_user_id' => $userId,
+//                         'created_at' => date('Y-m-d H:i:s',time()-(rand(1,100000))),
+//                         'updated_at' => date('Y-m-d H:i:s',time())
+//                     ]);
+//                     $userArr[] = $userId;
+//                 }
+//             }
+//
+//             for ($i = 0;$i<$value['id']%10;$i++){
+//                 $userArr[] = rand(9990,10187);
+//             }
+//             foreach ($userArr as $user){
+//                 \App\Models\Topic\TopicContentLike::query()->firstOrCreate([
+//                     'content_id' => $value['id'],
+//                     'mer_user_id' => $user
+//                 ]);
+//                 \App\Models\User\MerUserFollow::query()->firstOrCreate([
+//                     'follow_user_id' => $value['mer_user_id'],
+//                     'mer_user_id' => $user
+//                 ]);
+//             }
 
          }
      });
