5463cc5162c0ebf7f8623035a9dd2c8b3a6665a7
xurf <375634210@qq.com>
2021-03-17 16:46:44 +0800

    热门推荐随机刷新

--- app/Base/helpers.php
+++ app/Base/helpers.php
@@ -492,3 +492,20 @@ function getIp2($ip = null)
     }
     return [];
 }
+
+/**
+ * 多维数组排序
+ * @param $data
+ * @param $sort_order_field
+ * @param int $sort_order
+ * @param int $sort_type
+ * @return mixed
+ */
+function my_array_multisort($data,$sort_order_field,$sort_order=SORT_ASC,$sort_type=SORT_NUMERIC){
+    foreach($data as $val){
+        $key_arrays[]=$val[$sort_order_field];
+    }
+    array_multisort($key_arrays,SORT_ASC,SORT_NUMERIC,$data);
+    return $data;
+}
+
