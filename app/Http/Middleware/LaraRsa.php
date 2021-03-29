c24cb6b99ab1ddb7f3de7855ff73c2bcd4f6bd27
xurf <375634210@qq.com>
2021-03-29 16:19:10 +0800

    积分上报去除时间

--- app/Http/Middleware/LaraRsa.php
+++ app/Http/Middleware/LaraRsa.php
@@ -36,6 +36,7 @@ class LaraRsa
             throw new \Exception('params empty.');
         }
         $result = json_decode($result,true);
+        //客户端获取的是设备当前时间
 //        if (isset($result['millis']) && (time() - $result['millis'] >= $this->timeOut)){
 //            throw new \Exception('timeout error.');
 //        }
