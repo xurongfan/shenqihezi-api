04265ba3d8a41d0944d8593a7bb3d8debf25cac2
xurf <375634210@qq.com>
2021-04-13 18:27:13 +0800

    fix bug

--- app/Base/helpers.php
+++ app/Base/helpers.php
@@ -389,7 +389,7 @@ function findNum($str=''){
  */
 function makeOrderNumber()
 {
-    $order_id_main = date('YmdHis') . rand(100000,999999);
+    $order_id_main = date('YmdHis') . rand(1000,9999);
 
     $order_id_len = strlen($order_id_main);
 
